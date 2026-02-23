df1e0011098cfa5f550f16ebfe1128330d857d3e
mcenci24 <mrc6379@psu.edu>
2026-02-22 21:38:31 -0500

    added counter

--- my-element.js
+++ my-element.js
@@ -9,6 +9,8 @@ import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
 /**
  * `my-element`
  * 
+ * Minimal counter element.
+ * 
  * @demo index.html
  * @element my-element
  */
@@ -20,12 +22,15 @@ export class MyElement extends DDDSuper(I18NMixin(LitElement)) {
 
   constructor() {
     super();
-    this.title = "";
+    this.title = "Counter App";
+    this.count = 0; // Reactive counter value
+
     this.t = this.t || {};
     this.t = {
       ...this.t,
       title: "Title",
     };
+
     this.registerLocalization({
       context: this,
       localesPath:
@@ -39,36 +44,54 @@ export class MyElement extends DDDSuper(I18NMixin(LitElement)) {
     return {
       ...super.properties,
       title: { type: String },
+      count: { type: Number }, // counter is reactive
     };
   }
 
   // Lit scoped styles
   static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--my-element-label-font-size, var(--ddd-font-size-s));
-      }
-    `];
+    return [
+      super.styles,
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-primary);
+          background-color: var(--ddd-theme-accent);
+          font-family: var(--ddd-font-navigation);
+        }
+        .wrapper {
+          margin: var(--ddd-spacing-2);
+          padding: var(--ddd-spacing-4);
+        }
+      `,
+    ];
   }
 
   // Lit render the HTML
   render() {
     return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
+      <div class="wrapper">
+        <h3><span>${this.t.title}:</span> ${this.title}</h3>
+        <p>Count: ${this.count}</p>
+        <button @click="${this.increment}">Increment</button>
+        <button @click="${this.decrement}">Decrement</button>
+        <slot></slot>
+      </div>
+    `;
+  }
+
+  /**
+   * Increase the counter by 1
+   */
+  increment() {
+    this.count += 1;
+  }
+
+  /**
+   * Decrease the counter by 1
+   */
+  decrement() {
+    this.count -= 1;
   }
 
   /**
